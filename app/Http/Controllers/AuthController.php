a70705c70bdda10a71eb2a000e4e8d24cc84ecc2
FecGbo <gbot22633@gmail.com>
2025-07-30 18:01:37 +0630

    responsive

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
         if (Auth::attempt($credentials)) {
             $user = Auth::user();
 
-            $previousLogin = $user->last_login;
+
             $user->last_login = now();
 
 
